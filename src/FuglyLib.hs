139aefbd2f985e947f8f19ab985667dd0f5107e7
Tully Gray <shadowdaemon@anapnea.net>
2014-10-20 17:38:15 +1100

    Remove old commented code.

--- src/FuglyLib.hs
+++ src/FuglyLib.hs
@@ -769,15 +769,6 @@ gfParseBoolA pgf' msg
     m = unwords msg
     lang = head $ languages pgf'
 
-{--
-gfParseBool2 :: PGF -> String -> Bool
-gfParseBool2 pgf msg = lin pgf lang (parse_ pgf lang (startCat pgf) Nothing msg)
-  where
-    lin pgf lang (ParseOk tl, _)      = True
-    lin pgf lang _                    = False
-    lang = head $ languages pgf
---}
-
 gfParseC :: PGF -> String -> [String]
 gfParseC pgf' msg = lin pgf' lang (parse_ pgf' lang (startCat pgf') Nothing msg)
   where
